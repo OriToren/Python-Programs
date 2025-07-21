e5ba5e07d3a1aa2d97a1f6fc7d1a71e156bb9b86
ori toren <oritoren65@gmail.com>
2025-07-22 02:36:36 +0300

    start of reinvention

--- MyPackage/eln.py
+++ MyPackage/eln.py
@@ -1,27 +1,4 @@
-import quadratic
-import random
-class exponentException(Exception):
-    pass
-class InvalidFormatException(exponentException):
-    def __init__(self):
-        super().__init__("Wrong format for exponent function")
 
-def ePower(x):
-    # using ePower(ln(x)) == x breaks for some reason even tho for the same x ePower(x) works #fixed.
-    sum=0
-    mark=1
-    try:
-      for i in range(1000):
-           sum+=((x**i) / mark)
-           if i>=1:
-               mark*=i+1
-      if x>=0:
-        return round(sum,5)
-      else:
-        return round(sum,5)
-    except(OverflowError):
-         if x>=0:
-            return round(sum,5)
 def ln(x):
     if x<0:
         return None
@@ -42,102 +19,153 @@ def ln(x):
     for i in range(1,1000):
         sum=sum+((x-1)**i / i )*(-1)**(i+1)
     return round(sum,5)
+def ePower(x):
+    sum=0
+    mark=1
+    try:
+      for i in range(1000):
+           sum+=((x**i) / mark)
+           if i>=1:
+               mark*=i+1
+           if round(sum,5) == round(sum-((x**i)/mark),5):
+               break
+      if x>=0:
+        return round(sum,5)
+      else:
+        return round(sum,5)
+    except(OverflowError):
+         if x>=0:
+            return round(sum,5)
+class ELNException(Exception):
+    pass
 
+class InvalidCombinationException(ELNException):
+    def __init__(self,msg):
+        super().__init__(msg)
+class NotSupportedException(ELNException):
+    def __init__(self,msg):
+        super().__init__(msg)
+class Constant: #any constant
+    def __init__(self,num):
+        self.num=num
+    def getnum(self):
+        return self.num
+    def evaluate(self,anynum):
+        return self.num
+    def getderivative(self):
+        return Constant(0)
+    def tostring(self):
+        return str(self.num)
+    def add(self,num):
+      if (isinstance(num,Constant)):
+        return Constant(self.num+num.num)
+      else:
+          NotImplemented
+    def integral(self):
+        return Mull(Constant(self.num),Var())
 
-def lnList(items):
-    for item in range(len(items)):
-        if isinstance(items[item],float,int) and items[item] > 0:
-         if ln(items[item]) != None:
-          items[item]=ln(items[item])
-         else:
-            return False
+class Var: #x
+    def evaluate(self,num):
+        return num
+    def getderivative(self):
+        return Constant(1)
+    def tostring(self):
+        return "x"
+    def integral(self):
+        integral=Mull(Constant(1/2),Expo(Var(),Constant(2)))
+        return integral
+    def getnum(self):
+        return 0
+class Function:
+    def __init__(self,base,fx,gx):
+         self.base=base
+         self.fx=fx
+         self.gx=gx
+    def getbase(self):
+        return self.base.tostring()
+    def getexpo(self):
+        return self.expo.tostring()
+    def getfx(self):
+        return self.gx.tostring()
+    def tostring(self):
+        return f"{self.base.tostring()}^({self.fx.tostring()}) * {self.gx.tostring()}"
+    def __str__(self):
+        return self.tostring()
+class Add:
+    def __init__(self,firstpart,secendpart):
+        self.firstpart=firstpart
+        self.secendpart=secendpart
+    def evaluate(self,x_value):
+        if (type(x_value) == int or type(x_value) == float):
+         return self.firstpart.evaluate(x_value) + self.secendpart.evaluate(x_value)
         else:
-            return False
-    return items
-"""
- if isinstance(a,tuple):
-        if len(a) == 3:
-            temp = a
-            a = temp[0]
-            fx = temp[1]
-            gx = temp[2]
-    try:
-        if _check_eln(a, fx, gx) == False:
-            raise InvalidFormatException
-    except InvalidFormatException as e:
-        print(e)
-        return
-"""
-"""
-def _check(a,fx,gx):
-    if (type(a) != float and type(a) != int) or (type(fx) != tuple) or (type(gx) != tuple and type(gx) != int and type(gx) != float):
-        return False
-    if quadratic._check(fx) == False:
-        return False
-    if type(gx)== tuple and quadratic._check(gx) == False:
-        return False
-    return True
-def getFunc(number:float,exponent:tuple,times_this:tuple):
-    try:
-     if _check_eln(number,exponent,times_this) == True:
-      return (number,exponent,times_this)
-     else:
-         raise InvalidFormatException()
-    except InvalidFormatException as e:
-        print(e)
-        return
-def Roots(a:float,fx:tuple=None,gx:tuple=None):
-    if isinstance(a, tuple):
-        if len(a) == 3:
-            temp = a
-            a = temp[0]
-            fx = temp[1]
-            gx = temp[2]
-    try:
-        if _check_eln(a, fx, gx) == False:
-            raise InvalidFormatException
-    except InvalidFormatException as e:
-        print(e)
-        return
-    return quadratic.Roots(gx) #a^f(x) cant be zero
-def Derivative(a:float,fx:float=None,gx:tuple or float = None):
-    if isinstance(a, tuple):
-        if len(a) == 3:
-            temp = a
-            a = temp[0]
-            fx = temp[1]
-            gx = temp[2]
-    try:
-        if _check_eln(a, fx, gx) == False:
-            raise InvalidFormatException
-    except InvalidFormatException as e:
-        print(e)
-        return
-    if type(gx) != tuple:
-        return (a,fx,quadratic.mullFunc(quadratic.Derivative(fx),ln(a)))
-"""
-def getFunc(A,exponent,timesfunc):
-  try:
-   if _check(A,exponent,timesfunc) == True:
-    fx=exponent
-    gx=timesfunc
-    if type(gx) == tuple:
-     func=(f"({A}^({fx[0]}x^2+{fx[1]}x+{fx[2]})*({gx[0]}x^2+{gx[1]}x+{gx[2]}))")
-     return func
-    else:
-     func=(f"({A}^({fx[0]}x^2+{fx[1]}x+{fx[2]}))*{gx}")
-     return func
-   else:
-       raise InvalidFormatException()
-  except InvalidFormatException as e:
-      print(e)
-      return
-def _check(a,fx,gx):
-    if (type(a) != float and type(a) != int) or (type(fx) != tuple) or (type(gx) != tuple and type(gx) != int and type(gx) != float):
-        return False
-    if quadratic._check(fx[0],fx[1],fx[2]) == False:
-        return False
-    if type(gx)== tuple and quadratic._check(gx[0],gx[1],gx[2]) == False:
-        return False
-    return True
-print(getFunc(2,(0,1,0),(1,1,1)))
+            raise InvalidCombinationException("evaluate doesnt accept non number types")
+    def getderivative(self):
+        return Add(self.firstpart.getderivative(),self.secendpart.getderivative())
+    def tostring(self):
+        return f"{self.firstpart.tostring()} + {self.secendpart.tostring()}"
+    def __str__(self):
+        return self.tostring()
+    def integral(self):
+        return Add(self.firstpart.integral(),self.secendpart.integral())
+class Mull:
+    def __init__(self,firstpart,secendpart):
+        self.firstpart=firstpart
+        self.secendpart=secendpart
+    def evaluate(self,x_value):
+
+            if (type(x_value) == int or type(x_value) == float):
+                return self.firstpart.evaluate(x_value) * self.secendpart.evaluate(x_value)
+            else:
+                raise InvalidCombinationException("evaluate doesnt accept non number types")
+    def getderivative(self):
+        if (isinstance(self.firstpart,Constant) and isinstance(self.secendpart,Constant)) == True: #edgecases
+            return Constant(0)
+        if (isinstance(self.firstpart,Constant) and isinstance(self.secendpart,Var)) == True:
+            return Constant(self.firstpart.getnum())
+        if (isinstance(self.firstpart,Var) and isinstance(self.secendpart,Constant)) == True:
+            return Constant(self.secendpart.getnum())
+        return Add(Mull(self.firstpart.getderivative(), self.secendpart),Mull(self.firstpart, self.secendpart.getderivative()))
+ #   def integral(self):
+#        return Add(Mull(self.firstpart,self.secendpart.integral()),integral(Mull(self.firstpart.getderivative(),self.secendpart.integral()))) #workon this tomorow
+
+    def tostring(self):
+        return f"{self.firstpart.tostring()}*{self.secendpart.tostring()}"
+    def __str__(self):
+        return self.tostring()
+class Expo:
+    def __init__(self,base,exponent):
+        self.base=base
+        self.exponent=exponent
+    def evaluate(self,x_value):
+        if (type(x_value) == int or type(x_value) == float):
+            return self.base.evaluate(x_value) ** self.exponent.evaluate(x_value)
+        else:
+            raise InvalidCombinationException("evaluate doesnt accept non number types")
+    def getderivative(self):
+        if (isinstance(self.base,Constant) and isinstance(self.exponent,Constant)) == True: #edgecases
+            return Constant(0)
+        if (isinstance(self.base, Constant) and isinstance(self.exponent, Var)) == True:
+            return Mull(self, Constant(ln(self.base.getnum())))
+        if (isinstance(self.base,Var) and isinstance(self.exponent, Constant)) == True:
+            return Mull(Expo(self.base,Constant(self.exponent.getnum()-1)),Constant(self.exponent.getnum()))
+    def tostring(self):
+        return  f"{self.base.tostring()}^({self.exponent.tostring()})"
+    def __str__(self):
+        return self.tostring()
+    def integral(self):
+        if type(self.base)==Constant and type(self.exponent) == Var: #replace the number with ln later
+            return Mull(Expo(self.base,self.exponent),Constant(ln(self.base.getnum())))
+        if type(self.base) == Constant and type(self.exponent) == Constant:
+            return Mull(Expo(self.base,self.exponent),Var())
+        if self.exponent.tostring() != "x":
+            return Mull(Constant(1/(self.exponent.getnum()+1)),Expo(Var(),Constant(self.exponent.getnum()+1)))
+        try:
+         raise NotSupportedException("we dont support x^x integrals or derivatives")
+        except NotSupportedException as e:
+             print(e)
+             return
+#print(Add(Constant(3),Mull(Constant(3),Var())))
+#print(Mull(Consant(3),Var()))
+func=Mull(Expo(Constant(3),Var()),Var())
+print(func.getderivative())
